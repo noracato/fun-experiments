6cbd40a2312b57bbd6c610a1444097315aa5f8b0
noracato <ncschinkel@gmail.com>
2023-03-09 15:32:30 +0100

    Add support for multiple maps

--- negative/negative.js
+++ negative/negative.js
@@ -1,12 +1,24 @@
 $( document ).ready(start);
 
-let localStorageItemName = "R-take-a-guess";
+let localStorageItemName = "R-take-a-guess-";
 let currentStorage = [];
+let currentMap = 1;
+const totalMaps = 3;
+
+let lockNext = false
 
 function start(){
     setUpStorage();
     $('#submit').on('click', addAnswer);
     $('#hide-results').on('click', transparency);
+    $('#next').on('click', nextMap);
+    $('#letters').keypress(function (e) {
+        if (e.which == 13) { // the enter key code
+           addAnswer();
+           return false;
+        }
+    });
+    setTimeout(function(){lockNext = false;}, 500);
 }
 
 function addAnswer() {
@@ -14,8 +26,9 @@ function addAnswer() {
     let newAnswer = $('<p>'+newValue+'</p>');
     $('#letters')[0].value = '';
     $('#results').prepend(newAnswer);
+    $('#letters')[0].focus();
     currentStorage.push(newValue);
-    localStorage.setItem(localStorageItemName, currentStorage);
+    localStorage.setItem(localStorageItemName+currentMap, currentStorage);
 }
 
 function transparency() {
@@ -27,12 +40,27 @@ function transparency() {
 }
 
 function setUpStorage() {
-    let st = localStorage.getItem(localStorageItemName);
+    $('#results').children().remove();
+    let st = localStorage.getItem(localStorageItemName+currentMap);
     if (st) {
         currentStorage = st.split(',');
         for (var i=0; i < currentStorage.length; i++){
             let newAnswer = $('<p>'+currentStorage[i]+'</p>');
             $('#results').prepend(newAnswer);
         }
+    } else {
+        currentStorage = [];
     }
+}
+
+function nextMap(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (lockNext) {return;}
+    lockNext = true;
+
+    currentMap += 1;
+    if (currentMap > totalMaps) { currentMap = 1};
+    $('img.map').attr('src', 'assets/map'+currentMap+'.png');
+    start();
 }
