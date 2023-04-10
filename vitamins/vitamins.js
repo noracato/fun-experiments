20c2f876ef4ce8b9398e3ef68fe868555afc865d
noracato <ncschinkel@gmail.com>
2023-04-10 17:44:47 +0200

    With clock

--- vitamins/vitamins.js
+++ vitamins/vitamins.js
@@ -2,6 +2,10 @@ $( document ).ready(start);
 
 function start(){
     $('.to-swallow').click(swallow);
+
+    if ($('#timer').length) {
+        countDown()
+    }
 }
 
 function emptyScreen(){
@@ -63,4 +67,35 @@ function movieScreen() {
             $('body').append($('<div class="soon">Starting soon</div>'));
         }, 3000)
     }, 3000)
+}
+
+
+function countDown() {
+    var countDownDate = new Date("Apr 11, 2023 21:37:25").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+    }, 1000);
 }
