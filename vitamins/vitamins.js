091c30568fe1333ca90d416ece841557954c2fa7
noracato <ncschinkel@gmail.com>
2023-04-11 17:55:23 +0200

    Rename artwork to performance

--- vitamins/vitamins.js
+++ vitamins/vitamins.js
@@ -71,7 +71,7 @@ function movieScreen() {
         $('.fill-up').addClass('fall-down');
         $('#empty').addClass('swallowed');
         setTimeout(function() {
-            $('body').append($('<div class="soon">Artwork by Nicolette Ni </br> Starting soon</div>'));
+            $('body').append($('<div class="soon">Performance by Nicolette Ni</div>'));
         }, 3000)
     }, 3000)
 }
